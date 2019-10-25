f291a6b8bb2cd766afa4b228e6dfd95f6c78d820
burbage stacey <rstacey06@gmail.com>
2019-10-25 15:54:56 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,4 +9,21 @@ def map_to_negativize(source_array)
 end
 
 def map_to_no_change(source_array)
-  
+  new = []
+  i = 0
+  while i < source_array.length do
+    new.push( source_array[i] )
+    i += 1
+  end
+  return new
+end
+
+def map_to_double(source_array)
+  new[]
+  i = 0 
+  while i < source_array.length do 
+    new.push( source_array[i] ) * 2
+    i  += 1 
+  end 
+  return new
+end 
