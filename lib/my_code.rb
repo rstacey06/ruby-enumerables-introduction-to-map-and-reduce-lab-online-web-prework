12f0f8bd4f7e78fd310dc4508768512042a1ccea
burbage stacey <rstacey06@gmail.com>
2019-10-25 16:00:56 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -19,7 +19,14 @@ def map_to_no_change(source_array)
 end
 
 def reduce_to_total(source_array, starting_point)
-  
+  new = starting_point
+  i = 0
+  while i < source_array.length do
+    new += source_array[i]
+    i += 1
+  end
+  return new
+end
 
 def map_to_double(source_array)
   new = []
