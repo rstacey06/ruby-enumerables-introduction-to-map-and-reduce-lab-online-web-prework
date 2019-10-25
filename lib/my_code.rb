34b3b2f5f5979662cdea866f29cac4c936962810
burbage stacey <rstacey06@gmail.com>
2019-10-25 16:05:26 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -18,7 +18,27 @@ def map_to_no_change(source_array)
   return new
 end
 
-def reduce_to_total(source_array, starting_point)
+def map_to_double(source_array)
+  new = []
+  i = 0
+  while i < source_array.length do
+    new.push( source_array[i] * 2 )
+    i += 1
+  end
+  return new
+end
+
+def map_to_square(source_array)
+  new = []
+  i = 0
+  while i < source_array.length do
+    new.push( source_array[i] * source_array[i] )
+    i += 1
+  end
+  return new
+end
+
+def reduce_to_total(source_array, starting_point=0)
   new = starting_point
   i = 0
   while i < source_array.length do
@@ -38,34 +58,11 @@ def reduce_to_all_true(source_array)
 end
 
 def reduce_to_any_true(source_array)
-  i = 0 
-  while i < source_array.length do 
-    return true if !source_array[i]
-    i += 0 
-  end
-  return false 
-end
-
-
-def map_to_double(source_array)
-  new = []
-  i = 0 
-  while i < source_array.length do 
-    new.push( source_array[i] * 2)
-    i += 1 
-  end 
-  return new
-end 
-
-def map_to_square(source_array)
-  new =[]
-  i = 0 
+  i = 0
   while i < source_array.length do
-    new.push(source_array[i] * source_array[i])
-    i += 1 
-  end 
-  return new 
+    return true if source_array[i]
+    i += 1
+  end
+  return false
 end
-
-
 
