849813ddba39aa1ff3ad3fac6c01ccd5f8d9668d
burbage stacey <rstacey06@gmail.com>
2019-10-25 15:56:45 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -22,7 +22,7 @@ def map_to_double(source_array)
   new = []
   i = 0 
   while i < source_array.length do 
-    new.push( source_array[i] ) * 2)
+    new.push( source_array[i] * 2)
     i += 1 
   end 
   return new
