24ec01a2b4d2bb5f9650216caafc47b187fe051b
burbage stacey <rstacey06@gmail.com>
2019-10-25 16:02:29 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -29,6 +29,13 @@ def reduce_to_total(source_array, starting_point)
 end
 
 def reduce_to_all_true(source_array)
+  i = 0
+  while i < source_array.length do
+    return false if !source_array[i]
+    i += 1
+  end
+  return true
+end
 
 
 def map_to_double(source_array)
